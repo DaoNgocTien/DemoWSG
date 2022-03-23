a922494d00a79da6b706979d8f577c5983555361
ngothuonghoanganh <ngothuonghoanganh1144@gmail.com>
2022-03-23 22:44:55 +0700

    update profile

--- src/components/Profile/views/main-view.js
+++ src/components/Profile/views/main-view.js
@@ -1,54 +1,20 @@
-import React, { Component, memo } from "react";
-import { Route, Link, Redirect } from "react-router-dom";
 import {
-  Table,
-  Button,
-  Input,
-  Row,
-  Col,
-  PageHeader,
-  Space,
-  Form,
-  Select,
-  InputNumber,
-  Switch,
-  Radio,
-  Slider,
-  Upload,
-  Rate,
-  Checkbox,
-  Avatar,
-  Descriptions,
-  Tag,
-  Statistic,
-  Tabs,
-} from "antd";
+  IdcardTwoTone,
+  SafetyCertificateTwoTone,
+  WalletTwoTone,
+} from "@ant-design/icons";
+import { PageHeader, Select, Tabs, Tag } from "antd";
 import moment from "moment";
 import PropTypes from "prop-types";
-import { UserOutlined, WalletTwoTone, IdcardTwoTone, SafetyCertificateTwoTone } from '@ant-design/icons';
-
-
-import ProfileTab from "./profile-view";
+import React, { Component, memo } from "react";
+import { Link } from "react-router-dom";
 import EWalletTab from "./e-wallet-view";
+import ProfileTab from "./profile-view";
 import PasswordTab from "./security-view";
 
-import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
 const { Option } = Select;
 const { TabPane } = Tabs;
 
-const IconLink = ({ src, text, link }) => (
-  <a className="example-link">
-    <img className="example-link-icon" src={src} alt={text} style={{ marginRight: "5px" }} />
-    <Link
-      className="LinkDecorations"
-      to={link}
-    >
-      {text}
-    </Link>
-  </a>
-);
-
-//  prototype
 const propsProTypes = {
   index: PropTypes.number,
   data: PropTypes.array,
@@ -58,7 +24,6 @@ const propsProTypes = {
   deleteCampaign: PropTypes.func,
 };
 
-//  default props
 const propsDefault = {
   index: 1,
   data: [],
@@ -77,7 +42,7 @@ class ProfileUI extends Component {
       rolename: "",
     },
     loading: false,
-    selectedRowKeys: [], // Check here to configure the default column
+    selectedRowKeys: [],
     loadingActionButton: false,
     editButton: false,
     deleteButton: false,
@@ -97,10 +62,10 @@ class ProfileUI extends Component {
     this.setState({
       user: {
         username: storedUser.username,
-        googleId:storedUser.googleid,
-        loginMethod: storedUser.googleid ? "BY GOOGLE MAIL": "BY USERNAME",
-      ...this.props.data
-      }
+        googleId: storedUser.googleid,
+        loginMethod: storedUser.googleid ? "BY GOOGLE MAIL" : "BY USERNAME",
+        ...this.props.data,
+      },
     });
   }
 
@@ -119,7 +84,7 @@ class ProfileUI extends Component {
   start = (openModal) => {
     let selectedRowKeys = this.state.selectedRowKeys;
     let data = this.props.data;
-    //  Get campaign record
+
     let recordToEdit = data.filter((item) => {
       return selectedRowKeys.includes(item.id);
     })[0];
@@ -143,14 +108,12 @@ class ProfileUI extends Component {
 
         break;
       case "openOrdersInCampaign": {
-        //  Get orders in campaign
         let orderList = this.props.orderList;
         let orderListInCampaign = orderList?.filter((item) => {
           return selectedRowKeys.includes(item.campaignid);
         });
         this.props.getCampaign(selectedRowKeys);
 
-        //  Set campaign record and orders in campaign into state
         this.setState({
           openDrawer: true,
           record: recordToEdit,
@@ -252,19 +215,10 @@ class ProfileUI extends Component {
   };
 
   onSelectChange = (selectedRowKeys) => {
-    // console.log("selectedRowKeys changed: ", selectedRowKeys);
-    // console.log(this.props.data);
     let record = this.props.data.filter((item) => {
       return selectedRowKeys.includes(item.id);
     })[0];
 
-    // console.log(record);
-    // this.setState({
-    //   record: this.props.data.filter((item) => {
-    //     return selectedRowKeys.includes(item.id);
-    //   })[0]
-    // });
-    // // console.log(this.state.record);
     this.setState({
       selectedRowKeys,
       record: record,
@@ -275,11 +229,11 @@ class ProfileUI extends Component {
   };
 
   onFinish = (values) => {
-    console.log('Received values of form: ', values);
+    console.log("Received values of form: ", values);
   };
 
   normFile = (e) => {
-    console.log('Upload event:', e);
+    console.log("Upload event:", e);
 
     if (Array.isArray(e)) {
       return e;
@@ -288,32 +242,15 @@ class ProfileUI extends Component {
     return e && e.fileList;
   };
 
-
-
   render() {
-    const {
-      selectedRowKeys,
-      deleteButton,
-      editButton,
-      addNewButton,
-      openCreateModal,
-      openDeleteModal,
-      openEditModal,
-      displayData,
-      searchKey,
-      user
-    } = this.state;
+    const { selectedRowKeys, user } = this.state;
 
     const { productList, createCampaign, updateCampaign, deleteCampaign } =
       this.props;
 
-    const rowSelection = {
-      selectedRowKeys,
-      onChange: this.onSelectChange,
-    };
     console.log(this.props.data);
     console.log(user);
-    // const hasSelected = selectedRowKeys.length > 0;
+
     const arrayLocation = window.location.pathname.split("/");
     return (
       <PageHeader
@@ -321,138 +258,57 @@ class ProfileUI extends Component {
         className="site-page-header-responsive"
         onBack={() => window.history.back()}
         title={arrayLocation[1].toUpperCase()}
-        // subTitle={`This is a ${arrayLocation[1]} page`}
         tags={
           <>
             <Tag color="blue">{this.props.data.rolename}</Tag>
             <Tag color="red">{user.loginMethod}</Tag>
           </>
         }
-        // avatar={
-        //   <Avatar
-        //     shape="square"
-        //     size={100}
-        //     icon={<UserOutlined />}
-        //   />
-        // }
         footer={
           <Tabs defaultActiveKey="profile" centered type="card">
-            <TabPane tab={
-              <span>
-                <IdcardTwoTone style={{ fontSize: "25px" }} />
-                Profile
-              </span>
-            }
+            <TabPane
+              tab={
+                <span>
+                  <IdcardTwoTone style={{ fontSize: "25px" }} />
+                  Profile
+                </span>
+              }
               key="profile"
               style={{ background: "#ffffff" }}
             >
               <div>
-
-
                 <ProfileTab />
-
               </div>
             </TabPane>
 
-            <TabPane tab={
-              <span style={{ alignItems: "center" }}>
-                <SafetyCertificateTwoTone style={{ fontSize: "25px" }} />
-                Security
-              </span>
-            }
+            <TabPane
+              tab={
+                <span style={{ alignItems: "center" }}>
+                  <SafetyCertificateTwoTone style={{ fontSize: "25px" }} />
+                  Security
+                </span>
+              }
               key="security"
               style={{ background: "#ffffff" }}
             >
-              <PasswordTab
-              // changePassword={this.props.changePassword}
-              />
+              <PasswordTab />
             </TabPane>
 
-            <TabPane tab={
-              <span>
-                <WalletTwoTone style={{ fontSize: "25px" }} />
-                E-Wallet
-              </span>
-            }
+            <TabPane
+              tab={
+                <span>
+                  <WalletTwoTone style={{ fontSize: "25px" }} />
+                  E-Wallet
+                </span>
+              }
               key="ewallet"
               style={{ background: "#ffffff" }}
             >
               <EWalletTab />
             </TabPane>
           </Tabs>
-
         }
-      >
-        {/* <Descriptions size="small" column={10}>
-          <Descriptions.Item>
-            <Avatar
-              shape="square"
-              size={120}
-              icon={<UserOutlined />}
-            />
-          </Descriptions.Item>
-
-          <Descriptions.Item span={3}>
-            <Descriptions size="small" column={1}>
-              <Descriptions.Item label="Name">
-
-              </Descriptions.Item>
-              <Descriptions.Item label="Phone number">
-
-              </Descriptions.Item>
-              <Descriptions.Item label="Email">
-
-              </Descriptions.Item>
-              <Descriptions.Item>
-                <Space>
-                  <IconLink
-                    src="https://gw.alipayobjects.com/zos/rmsportal/MjEImQtenlyueSmVEfUD.svg"
-                    text="List orders"
-                    link="/orders/catalog"
-                  />
-                  <IconLink
-                    src="https://gw.alipayobjects.com/zos/rmsportal/NbuDUAuBlIApFuDvWiND.svg"
-                    text=" Product Info"
-                    link="/products/catalog"
-                  />
-                  <IconLink
-                    src="https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg"
-                    text="Income"
-                    link="/"
-                  />
-                </Space>
-              </Descriptions.Item>
-            </Descriptions>
-          </Descriptions.Item>
-
-          <Descriptions.Item span={2}>
-            <Statistic
-              title="Total Products"
-              suffix=" products"
-              value="100"
-            // style={{
-            //   marginRight: 32,
-            // }}
-            />
-          </Descriptions.Item>
-
-          <Descriptions.Item span={2}>
-            <Statistic
-              title="Total Campaigns"
-              suffix=" campaigns"
-              value="10"
-            // style={{
-            //   marginRight: 32,
-            // }}
-            />
-          </Descriptions.Item>
-          <Descriptions.Item span={2}>
-            <Statistic title="Orders" prefix="$" value={568.08} />
-          </Descriptions.Item>
-        </Descriptions> */}
-
-
-      </PageHeader >
+      ></PageHeader>
     );
   }
 }
@@ -461,5 +317,4 @@ const arePropsEqual = (prevProps, nextProps) => {
   return prevProps === nextProps;
 };
 
-// Wrap component using `React.memo()` and pass `arePropsEqual`
 export default memo(ProfileUI, arePropsEqual);
